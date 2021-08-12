b034c4596947b574cbcc8f67a14ef69b6d456854
HWcoms <dlgusdn383@gmail.com>
2021-08-13 06:05:26 +0900

    마우스 카메라 회전 기능 (basic)
    TpsFollowCam.cs 업데이트
    ㄴ마우스로 카메라 회전기능 (감도 설정, 가두기 기능)

--- Assets/Scripts/TpsFollowCam.cs
+++ Assets/Scripts/TpsFollowCam.cs
@@ -2,26 +2,47 @@
 
 public class TpsFollowCam : MonoBehaviour
 {
+    public bool isCursorLock = false;
+
+    public float MouseXSensitivity = 6f;
+    public float MouseYSensitivity = 6f;
+
     public Transform target;
-    public float lookAtOffset;  //offset for LookAt height
-
-    public Vector3 TempTarget;
-
+    private float lookAtOffset = -.75f;  //offset for LookAt height
+    private Vector3 TempTarget;
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
-    
 
     // Start is called before the first frame update
     void Start()
     {
+        if (isCursorLock)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (isCursorLock)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
 
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
@@ -31,6 +52,12 @@ public class TpsFollowCam : MonoBehaviour
         TempTarget = target.position;
         TempTarget -= transform.up * lookAtOffset;
 
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+
+        offset = Quaternion.AngleAxis(mouseX * MouseXSensitivity, Vector3.up) * offset;
+        offset = Quaternion.AngleAxis(mouseY * MouseYSensitivity, Vector3.left) * offset;
         transform.LookAt(TempTarget);
     }
 }
