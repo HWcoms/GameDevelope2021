da60c7c0fa60c2daf5af24bf62a41c40cd4ccf2e
HWcoms <dlgusdn383@gmail.com>
2021-08-13 05:12:14 +0900

    FollowCam 스크립트 업데이트 (smooth, LookAt)
    Lerp 이용한 카메라 부드럽게 따라가기 기능
    + 카메라 -> 플레이어 항상 바라보게 함

--- Assets/Scripts/TpsFollowCam.cs
+++ Assets/Scripts/TpsFollowCam.cs
@@ -3,19 +3,34 @@
 public class TpsFollowCam : MonoBehaviour
 {
     public Transform target;
+    public float lookAtOffset;  //offset for LookAt height
+
+    public Vector3 TempTarget;
+
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
-        transform.position = target.position + offset;
+        
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+
+        TempTarget = target.position;
+        TempTarget -= transform.up * lookAtOffset;
+
+        transform.LookAt(TempTarget);
     }
 }
