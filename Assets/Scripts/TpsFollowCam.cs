886eb4815d6790a8dfdbfa5fec98e2c01e8782d0
HWcoms <dlgusdn383@gmail.com>
2021-08-13 04:43:56 +0900

    1.캐릭터와 카메라 분리 2.카메라에 FollowScript 스크립트 작성
    1. 캐릭터가 뒤로 갈 때 카메라가 버그처럼 움직여서
    분리함
    2. 카메라에 기본적인 FollowScript 스크립트 (플레이어 위치를 따라감) 작성함

---
+++ Assets/Scripts/TpsFollowCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TpsFollowCam : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
