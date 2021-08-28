82aec4d1265052e4713d10aef35be08a66ef6f4c
HWcoms <dlgusdn383@gmail.com>
2021-08-28 18:28:36 +0900

    힐아이템 (인벤토리) E키로 작동
    캐릭터 힐아이템 (5개) e키로 회복가능

---
+++ Assets/Scripts/PlayerItemUsage.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerItemUsage : MonoBehaviour
+{
+    private CharacterHealth CHscript;
+    [SerializeField] private TextMeshProUGUI HealCountText;
+
+    [SerializeField] private int healItemCount = 5;
+    [SerializeField] private float healAmount = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CHscript = GetComponent<CharacterHealth>();
+
+        HealCountText = GameObject.Find("Heal Count").gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealCountText.text = getHealItemCount().ToString();
+
+        //use heal item
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            useHealItem(1);
+        }
+    }
+
+    public int getHealItemCount()
+    {
+        return healItemCount;
+    }
+
+    public void setHealItemCount(int value)
+    {
+        healItemCount = value;
+    }
+
+    public int useHealItem (int count)
+    {
+        /*
+        if (healItemCount <= 0)
+        {
+
+        }
+        */
+
+        healItemCount -= count;
+
+        CHscript.changeHp(healAmount);
+
+        return count;
+    }
+}
