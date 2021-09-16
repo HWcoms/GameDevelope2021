ac61306f7a4b72f17df28e2258644db13a5b149f
HWcoms <dlgusdn383@gmail.com>
2021-09-16 20:57:56 +0900

    EnemyHealth 스크립트 추가
    적 체력 + 피격 시스템

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private bool isDead = false;
+
+    [SerializeField] private float maxHp = 100.0f;
+    [SerializeField] private float maxStamina = 100.0f;
+
+    [SerializeField] private float hp = 100.0f;
+    [SerializeField] private float stamina = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (playerHP.getDead()) return;
+
+        //print(other.gameObject.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            if (isDealready)
+            {
+                if (playerHP.changeHp(-attackDamgage, 1))
+                {
+                    //hitParticle.GetComponentInChildren<TextMeshPro>().text = ((int)attackDamgage).ToString();
+                    hitParticleText.text = ((int)attackDamgage).ToString();
+
+                    isDealready = false;
+                    GameObject.Instantiate(hitParticle, this.GetComponentInChildren<Collider>().ClosestPointOnBounds(other.transform.position), transform.rotation);
+                }
+
+            }
+        }
+    }
+
+    public void Dead()
+    {
+        isDead = true;
+    }
+    public bool getDead()
+    {
+        return isDead;
+    }
+}
