64f332b92c785a8e194ae4b30f649bf38224ca74
KSH <kims6286@naver.com>
2021-09-22 18:44:17 +0900

    보스 움직임 태그(기본 - 이동), 일반 몬스터

---
+++ Assets/Spider_Queen/Assets/Prefab/Boss01_Move.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss01_Move : MonoBehaviour
+
+  
+{
+    private Animator boss1;
+    //private bool check;
+    private Transform playerTr;
+    private Transform BossTr;
+    
+    public float traceDist = 10.0f;
+    
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        boss1 = GetComponent<Animator>();
+        var player = GameObject.FindWithTag("Player");
+        if (player != null)
+            playerTr = player.GetComponent<Transform>();
+        //playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        BossTr = GetComponent<Transform>();
+        
+            
+    }
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(playerTr.position, BossTr.position);
+
+        if(dist<=traceDist)
+        {
+            boss1.SetBool("Is_Walk", true);
+        }
+        else
+        {
+            boss1.SetBool("Is_Walk", false);
+        }
+
+        /*if(GameObject.FindGameObjectsWithTag("Player"))
+        {
+
+        }
+        if(gameObject.CompareTag("Player"))
+        {
+            boss1.SetBool("Is_Walk", true);
+        }
+        else
+        {
+            boss1.SetBool("Is_Walk", false);
+        }*/
+    }
+}
---
+++ Assets/Spider_Queen/Assets/Prefab/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHp;
+    public int curHp;
+    // Start is called before the first frame update
+
+    Rigidbody rig;
+    BoxCollider box;
+    
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+        box = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
