766742a3138d0a0d860f03f17b9a885473f5b93e
AmbitionandVision <jch6775@naver.com>
2021-09-23 22:10:11 +0900

    스톤 오브젝트 추가, 스톤 떨어지는 속성 스크립트 적용

---
+++ Assets/ClickEvent.cs
@@ -0,0 +1,35 @@
+﻿//현재 클릭 시 이름으로 오브젝트를 찾는 소스
+
+using UnityEngine;
+using System.Collections;
+
+public class ClickEvent : MonoBehaviour {
+    public int ObstacleNumber = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {//마우스 좌측 클릭 발생 시.
+            GameObject tempObj = null;//임시 오브젝트 생성.
+            if (ObstacleNumber > 0 && ObstacleNumber < 6)
+            {
+                tempObj = GameObject.Find("Obstacle" + ObstacleNumber.ToString());
+                if (tempObj != null)
+                {//게임오브젝트를 성공적으로 받았다면.
+                    Debug.Log("성공적으로 " + tempObj.name + " 오브젝트를 받았습니다.");
+                }
+                else
+                {
+                    Debug.LogError(ObstacleNumber.ToString() + "번 몬스터를 얻는데 실패했습니다.");
+                }
+            }
+            else
+            {
+                Debug.LogError("잘못된 몬스터 번호입니다.");
+            }
+
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/StoneDamage.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneDamage : EnemySpinner
+{
+ 
+ CharacterHealth playerHP;
+
+    [SerializeField] private float attackDamgage = 25.0f;
+    [SerializeField] private GameObject hitParticle;
+
+    [Space(10)]
+    [Header("----------------------------- Dealable timer -----------------------------")]
+    [SerializeField] private bool isDealready = false;
+    [SerializeField] private float dealableTime = 0.3f;
+    [SerializeField] private float dealTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHP = GameObject.FindWithTag("Player").GetComponent<CharacterHealth>();
+        //hitParticle = (GameObject) Resources.Load("Scenes/Model/hit.prefab");
+        hitParticle.GetComponentInChildren<TextMesh>().text = ((int)attackDamgage).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.Rotate(0,0.3f,0,Space.Self);
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isDealready)
+        {
+            dealTimer -= Time.deltaTime;
+        }
+
+        if(dealTimer <= 0)
+        {
+            dealTimer = dealableTime;
+            isDealready = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (playerHP.getDead()) return;
+
+        print(other.gameObject.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            if(isDealready)
+            {
+                if(playerHP.changeHp(-attackDamgage))
+                {
+                    
+                    isDealready = false;
+                    GameObject.Instantiate(hitParticle, this.GetComponentInChildren<Collider>().ClosestPointOnBounds(other.transform.position) , transform.rotation);
+                }
+                    
+            }
+        }
+
+//     void OnCollisionEnter(Collider other)
+// {
+//   if(other.gameObject.tag == "Player")
+//     playerHP.changeHp(-25)
+// }
+
+
+    }
+
+
+
+
+
+
+
+
+// void OnCollisionEnter(Collider other)
+// {
+//   if(other.gameObject.tag == "Player")
+//     playerHP.changeHp(피해량)
+// }
+
+
+
+}
+
+
+
