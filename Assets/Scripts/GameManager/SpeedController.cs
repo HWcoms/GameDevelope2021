27bcd384be4c8171316867e10a2ceabcfd509275
HWcoms <dlgusdn383@gmail.com>
2021-09-25 00:44:22 +0900

    GameManager 오브젝트 추가, 게임 스피드 조정 스크립트 추가

---
+++ Assets/Scripts/GameManager/SpeedController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour
+{
+    [Range(0.1f, 2.0f)]
+    public float modifiedTimeSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = modifiedTimeSpeed;
+
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+            modifiedTimeSpeed -= 0.1f;
+        else if (Input.GetKeyDown(KeyCode.RightBracket))
+            modifiedTimeSpeed += 0.1f;
+        else if (Input.GetKeyDown(KeyCode.P))
+            modifiedTimeSpeed = 1.0f;
+
+        if (modifiedTimeSpeed <= 0.1f)
+            modifiedTimeSpeed = 0.1f;
+        else if (modifiedTimeSpeed >= 2.0f)
+            modifiedTimeSpeed = 2.0f;
+    }
+}
