c39ecd8f488aa9bdc8f75d1d10322e6ea2d48f28
HWcoms <dlgusdn383@gmail.com>
2021-10-22 21:16:18 +0900

    데미지 텍스트 표시 수정 (오버레이 안됨)
    데미지 텍스트 이제 보임 (화면 젤 앞에 표시 안됨)

--- Assets/Scenes/Model/hitNotification/TMPOverlay.cs
+++ Assets/Scenes/Model/hitNotification/TMPOverlay.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class billboardText : MonoBehaviour
+public class TMPOverlay : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+       GetComponentInChildren<TextMeshPro>().isOverlay = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Camera.main.transform.rotation;
+        
     }
 }
