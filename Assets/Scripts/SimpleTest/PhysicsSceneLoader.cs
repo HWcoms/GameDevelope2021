136cf9427d2a5db5940818ab14c29606bf1fd16b
HWcoms <dlgusdn383@gmail.com>
2021-10-24 03:38:22 +0900

    씬 병합 테스트 (캐릭터+맵)
    TestScene 1.1 + mapTest
    
    PostFX 수정 -> expose 16 -> 16.5
    
    PhysicsSceneLoader.cs 개발 중 (여러 씬 있을 때 충돌 처리)

---
+++ Assets/Scripts/SimpleTest/PhysicsSceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhysicsSceneLoader : MonoBehaviour
+{
+    public string physicsSceneName;
+    public float physicsSceneTimeScale = 1;
+    public PhysicsScene physicsScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadSceneParameters param = new LoadSceneParameters(LoadSceneMode.Additive, LocalPhysicsMode.Physics3D);
+        Scene scene = SceneManager.LoadScene(physicsSceneName, param);
+
+        physicsScene = scene.GetPhysicsScene();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(physicsScene != null)
+        {
+            physicsScene.Simulate(Time.fixedDeltaTime * physicsSceneTimeScale);
+        }
+    }
+}
