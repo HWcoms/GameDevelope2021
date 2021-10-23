e101af0adc036ae9726842c0075b7ba2b5c9b9d1
HWcoms <dlgusdn383@gmail.com>
2021-10-24 02:57:42 +0900

    데모맵 복제, 맵용 RP세팅, 심플 스크립트(캐릭터 움직임, 카메라)
    데모맵 복제,
    ㄴScenes/mapTest
    
     맵용 RP세팅
    ㄴScenes/AMapPostProcessingSettings,
    Scenes/AMapSkyandFogSettings
    
    심플 스크립트(캐릭터 움직임, 카메라)
    Scripts/SimpleTest/SimpleCamFollow.cs,
    Scripts/SimpleCharacterMovement.cs

---
+++ Assets/Scripts/SimpleTest/SimpleCamFollow.cs
@@ -0,0 +1,20 @@
+﻿// Description : Cam_Follow.cs : use on camera to follow the player character
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCamFollow : MonoBehaviour {
+    
+	public Transform 	target;
+	public float 		rotationDamping = 15;		
+
+	
+    void LateUpdate()
+    {
+        if(target != null){
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * rotationDamping);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime * rotationDamping); 
+        }
+      
+    }
+}
---
+++ Assets/Scripts/SimpleTest/SimpleCharacterMovement.cs
@@ -0,0 +1,73 @@
+﻿// Description : MouseController : use to controller where the character is look to.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleCharacterMovement : MonoBehaviour {
+    public CharacterController controller;
+
+    public Transform playerBody;
+    public Transform cam;
+    public float mouseSensitivity = 250f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float speed = 10f;
+    float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerBody = transform;
+        cam = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        yRotation -= mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, -yRotation, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
