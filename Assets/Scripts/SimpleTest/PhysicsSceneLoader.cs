ea37469fa1b46711cecbabc53e41a1abb699da72
HWcoms <dlgusdn383@gmail.com>
2021-10-25 00:45:30 +0900

    캐릭터 맵 아래에서 스폰되는 버그 고침
    PostProcesss, SkyAndFog 에 있는 Collider의 isTrigger -> True

--- Assets/Scripts/SimpleTest/PhysicsSceneLoader.cs
+++ Assets/Scripts/SimpleTest/PhysicsSceneLoader.cs
@@ -7,13 +7,35 @@ public class PhysicsSceneLoader : MonoBehaviour
     public float physicsSceneTimeScale = 1;
     public PhysicsScene physicsScene;
 
+    public bool isLoaded = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        Scene scene;
         LoadSceneParameters param = new LoadSceneParameters(LoadSceneMode.Additive, LocalPhysicsMode.Physics3D);
-        Scene scene = SceneManager.LoadScene(physicsSceneName, param);
 
-        physicsScene = scene.GetPhysicsScene();
+        if (SceneManager.sceneCount > 0)
+        {
+            for(int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene _scene = SceneManager.GetSceneAt(i);
+                if (_scene.name.Equals(physicsSceneName))
+                {
+                    print(_scene.name);
+                    physicsScene = _scene.GetPhysicsScene();
+                    isLoaded = true;
+                }
+            }
+        }
+
+        if (!isLoaded)
+        {
+            scene = SceneManager.LoadScene(physicsSceneName, param);
+            physicsScene = scene.GetPhysicsScene();
+
+            isLoaded = true;
+        }
     }
 
     // Update is called once per frame
