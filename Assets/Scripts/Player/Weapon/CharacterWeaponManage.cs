2e7d9d4c1dfce7c26bac0ddbdd3b5101579ceb41
HWcoms <dlgusdn383@gmail.com>
2021-10-31 19:53:06 +0900

    활 + 활 교체 + 무기 교체 관련( wip )
    망토 에셋 무시 gitignore 업데이트

--- Assets/Scripts/Player/Weapon/CharacterWeaponManage.cs
+++ Assets/Scripts/Player/Weapon/CharacterWeaponManage.cs
@@ -6,6 +6,18 @@ public class CharacterWeaponManage : MonoBehaviour
 {
     public List<GameObject> weapons = new List<GameObject>();
 
+    public AE_EffectAnimatorProperty Effect1;
+    [System.Serializable]
+    public class AE_EffectAnimatorProperty
+    {
+        [HideInInspector] public RuntimeAnimatorController TargetAnimation;
+        public GameObject Prefab;
+        public Transform BonePosition;
+        public Transform BoneRotation;
+        public float DestroyTime = 10;
+        [HideInInspector] public GameObject CurrentInstance;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
