3c6055c8f4732e05521937169e24bae34a55841a
HWcoms <dlgusdn383@gmail.com>
2021-11-01 21:53:06 +0900

    Scene 폴더이름 변경 -> _Scene, 망토 디테일 맵 추가(옷감)

---
+++ Assets/_Scenes/Model/hitNotification/TMPOverlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TMPOverlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       GetComponentInChildren<TextMeshPro>().isOverlay = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Scenes/Model/hitNotification/alphaFadeOut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class alphaFadeOut : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro tmpText;
+    public float fadeSpeed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpText = this.GetComponent<TextMeshPro>();
+    }
+
+    void textFadeOut()
+    {
+        StartCoroutine(fadeOutAnim(fadeSpeed, tmpText));
+    }
+
+    private IEnumerator fadeOutAnim(float timeSpeed, TextMeshPro text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime * timeSpeed));
+            yield return null;
+        }
+        Destroy(this.transform.parent.gameObject);
+    }
+}
