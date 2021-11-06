1233a069796ae80a44be525a56038811c964a072
HWcoms <dlgusdn383@gmail.com>
2021-11-06 19:42:28 +0900

    카메라 감도 6->3 (scene) + 공격 스태미나 소모 제거 + 리젠 FixedUpdate()
    카메라 감도 6->3 (scene 카메라 설정 변경)
    
    공격 스태미나 소모 제거
    ㄴCharacterActionScript.cs 의 공격스태미나비용 0, 애니메이션 이벤트에서도 ChangeStamina(0) 을 넘겨줌
    
    CharacterHealth.cs 수정
    ㄴhp, 스테미나 리젠함수 Upadate() 에서 FixedUpdate()로 변경 (프레임 속도에 상관없이 리젠됨)

--- Assets/Scripts/Player/CharacterHealth.cs
+++ Assets/Scripts/Player/CharacterHealth.cs
@@ -86,7 +86,7 @@ public class CharacterHealth : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         hpText.text = ((int)hp).ToString() + " / " + maxHp.ToString();
         staminaText.text = ((int)stamina).ToString() + " / " + maxStamina.ToString();
