19f6e303134686f907e275715301f06483ed43d3
HWcoms <dlgusdn383@gmail.com>
2021-12-10 05:32:41 +0900

    씬 조정 + 피격시 소울 생성 스크립트 (아무 Monster태그 달린 오브젝트에 사용가능 <- EnemyHealth.cs 필요)

--- Assets/Scripts/Enemy/EnemyHitSoul.cs
+++ Assets/Scripts/Enemy/EnemyHitSoul.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyHitSoul : MonoBehaviour
 {
+    EnemyHealth enemyhealthScript;
+    float temp_Hp;
+    public GameObject particlePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyhealthScript = GetComponent<EnemyHealth>();
+        temp_Hp = enemyhealthScript.getMaxHp();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //getHit
+        if (enemyhealthScript.getHp() < temp_Hp)
+        {
+            temp_Hp = enemyhealthScript.getHp();
+
+            GameObject soulPrefab = Instantiate(particlePrefab, GetComponent<EnemyTargetEffect>().GetEffectPos().position, Quaternion.identity);
+            soulPrefab.GetComponent<SoulParticle>().monster = this.gameObject;
+
+            //anim.SetBool("Is_Block", true);
+
+            //StartCoroutine(Block());
+        }
     }
 }
