2cf9a830e8a16bb4d944ef77e288b80889d202e9
HWcoms <dlgusdn383@gmail.com>
2022-03-10 15:45:24 +0900

    아래 커밋 스크립트 추가 + 벽 페이드 효과 추가 중 (1)

--- Assets/Scripts/GameManager/GameTutorialCollisionEvent.cs
+++ Assets/Scripts/GameManager/GameTutorialCollisionEvent.cs
@@ -10,7 +10,21 @@ public class GameTutorialCollisionEvent : MonoBehaviour
 
     [SerializeField] bool oncePlayed = false;
 
+
+    
+    public bool isDelay = true; //if not trigger by some condition
+
+    /*
+    public bool isKey = false;
+    public bool isCondition = false;
+    */
+
     public float delayTime = 3.0f;
+
+    private float wallAlpha = 0.0f;
+
+    public GameObject[] wallObjs;
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +47,19 @@ public class GameTutorialCollisionEvent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(GameObject obj in wallObjs)
+        {
+            /*
+            Color32 col = obj.material.GetColor("_Color");
+            col.a = wallAlpha;
+            obj.material.SetColor("_Color", col);*/
+        }
+
+        if(oncePlayed && !isDelay)  //if not control by delay
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+                StartCoroutine(offDisplay(0.5f));
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -47,9 +73,14 @@ public class GameTutorialCollisionEvent : MonoBehaviour
             foreach (GameObject childs in tutObjList)
             {
                 childs.SetActive(true);
+                //alpha
+                StartCoroutine(wallFadeIn());
             }
 
-            StartCoroutine(offDisplay(delayTime));
+            if(isDelay)
+            {
+                StartCoroutine(offDisplay(delayTime));
+            }
         }
     }
 
@@ -60,6 +91,17 @@ public class GameTutorialCollisionEvent : MonoBehaviour
         foreach (GameObject childs in tutObjList)
         {
             childs.SetActive(false);
+        }
+    }
+
+    IEnumerator wallFadeIn(float speed = 1.0f, float delay = 0.1f)
+    {
+        while(wallAlpha < 1.0f)
+        {
+            print(wallAlpha);
+
+            yield return new WaitForSeconds(delay);
+            wallAlpha += 0.01f * speed;
         }
     }
 }
