93f82237290d3ef59645ec6cf31fadc6267cf36e
HWcoms <dlgusdn383@gmail.com>
2022-03-23 22:00:00 +0900

    이벤트 시스템 메니저 + 이벤트 테스트용
    이벤트 시스템 메니저 (EventSystemManger.cs)
    ㄴ여러 이벤트 관리, 추가
    
    이벤트 테스트용 (TestEventGameObject.cs)
    ㄴ이벤트 게임오브젝트에 넣음, 오른쪽 방향키 누를시 이벤트 완료

---
+++ Assets/Scripts/EventSystem/EventSystemManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemManager : MonoBehaviour {
+
+    public GameObject[] eventList;
+    [SerializeField] private int currentEventIndex = 0;
+
+    [SerializeField] bool isEventRunable;
+
+    private bool isNext = false;
+    bool noEvent = false;
+
+    // Use this for initialization
+    void Start () {
+        isNext = false;
+        noEvent = false;
+        isEventRunable = true;
+
+        OffAllEvent();
+        CheckNextEvent();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (noEvent) { print("no event"); return; }
+
+        if(isEventRunable)
+            RunEvent();
+
+        if (isNext)
+            NextEvent();    //last check
+    }
+
+    void NextEvent()
+    {
+        if(CheckNextEvent())
+        {
+            currentEventIndex++;
+        }
+        isNext = false;
+        isEventRunable = true;
+    }
+
+    bool CheckNextEvent()
+    {
+        int eventSize = eventList.Length - 1;
+
+        if (eventSize <= currentEventIndex)
+        {
+            noEvent = true;
+            currentEventIndex = eventSize;
+
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    void OffAllEvent()
+    {
+        if (eventList.Length == 0) return;
+
+        foreach(GameObject eventObj in eventList)
+        {
+            eventObj.SetActive(false);
+        }
+    }
+
+    void RunEvent()
+    {
+        isEventRunable = false;
+
+        if (eventList[currentEventIndex] == null)
+        {
+            print("current event is null! skipping to next event");
+            return;
+        }
+
+        eventList[currentEventIndex].SetActive(true);
+    }
+
+    public void EndEvent()
+    {
+        int index = currentEventIndex;
+        //index = Mathf.Max(index, 0);
+
+        eventList[index].SetActive(false);
+        isNext = true;
+    }
+}
---
+++ Assets/Scripts/EventSystem/Test/TestEventGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEventGameObject : MonoBehaviour
+{
+    EventSystemManager esmObj;
+
+    void Start()
+    {
+
+        esmObj = GameObject.FindGameObjectWithTag("EventSystem").GetComponent<EventSystemManager>();
+    }
+
+    void Update()
+    {
+        //event end condition
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            esmObj.EndEvent();
+        }
+    }
+}
