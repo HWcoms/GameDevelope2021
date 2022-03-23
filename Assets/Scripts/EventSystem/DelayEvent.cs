b6f5839c054bf14c474a2365e306d3b6b7bc9d2c
HWcoms <dlgusdn383@gmail.com>
2022-03-23 22:02:19 +0900

    딜레이 이벤트
    이벤트 오브젝트 용
    ㄴ지정한 시간 후 다음 이벤트로 넘어감

---
+++ Assets/Scripts/EventSystem/DelayEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayEvent : MonoBehaviour
+{
+    public float delayTime = 0f;
+
+    EventSystemManager esmObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        esmObj = GameObject.FindGameObjectWithTag("EventSystem").GetComponent<EventSystemManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(timer(delayTime));
+    }
+
+    IEnumerator timer(float Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+        End();
+    }
+    void End()
+    {
+        esmObj.EndEvent();
+    }
+}
