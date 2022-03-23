e0f96e9a36fb2809fb4ccf9b145ade0951f8299a
HWcoms <dlgusdn383@gmail.com>
2022-03-23 23:51:38 +0900

    다이얼로그 이벤트 코드 작성중

---
+++ Assets/Scripts/EventSystem/GoogleSheetManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GoogleSheetManager : MonoBehaviour
+{
+    const string URL = "https://docs.google.com/spreadsheets/d/17l2xHpUDyDNRuh7rBE6A9BQmsleYnjUMd1S0OZfUM4g/export?format=tsv&range=A2:B";
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Start()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(URL);
+        yield return www.SendWebRequest();
+
+        string data = www.downloadHandler.text;
+        print(data);
+    }
+}
