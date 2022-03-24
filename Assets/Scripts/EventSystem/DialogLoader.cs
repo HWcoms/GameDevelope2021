79f2145a74e2d43634c47ecf384d828284169b60
HWcoms <dlgusdn383@gmail.com>
2022-03-24 20:40:07 +0900

    다이얼로그 스프레드시트 불러오기 시스템

---
+++ Assets/Scripts/EventSystem/DialogLoader.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogLoader : MonoBehaviour
+{
+    [SerializeField] private string sheetData;
+
+    public List<string> availableLanguages = new List<string>();
+
+    [System.Serializable]
+    public class Lang
+    {
+        public string lang;
+        public string Text, Button;
+    }
+
+    [System.Serializable]
+    public class Dialog
+    {
+        public int Id;
+        public string IdString, DilogType, Navigation;
+        public bool EndDialog;
+
+        public List<Lang> langInfo;
+    }
+
+    public List<Dialog> dilogList;
+
+    public static DialogLoader Singleton;
+    #region singleton
+    private void Awake()
+    {
+        if (null == Singleton)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this);
+        }
+        else Destroy(this);
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadSheet());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetInfoList (string data)
+    {
+        string[] row = data.Split('\n');
+        int rowSize = row.Length;
+        int columnSize = row[0].Split('\t').Length;
+        string[,] InfoArray = new string[rowSize, columnSize];
+
+        for(int i=0; i<rowSize; i++)
+        {
+            string[] column = row[i].Split('\t');
+            for (int j = 0; j < columnSize; j++) InfoArray[i, j] = column[j];
+        }
+        
+        int langCount = GetLanguageCount(InfoArray, columnSize);
+
+        dilogList = new List<Dialog>();
+        for (int i = 0; i < columnSize; i++)
+        {
+            Dialog tempDilog = new Dialog();
+            tempDilog.Id = int.Parse(InfoArray[i+1, 0]);
+            tempDilog.IdString = InfoArray[i+1, 1];
+
+            List<Lang> langList = new List<Lang>();
+            tempDilog.langInfo = langList;
+            //get Dilogs from available languages
+            for (int j = 0; j< langCount; j++)
+            {
+                Lang tempLang = new Lang();
+                tempLang.lang = availableLanguages[j];
+                tempLang.Text = InfoArray[i + 1, 5 + (j*2)];
+                tempLang.Button = InfoArray[i + 1, 5 + (j*2) + 1];
+
+                tempDilog.langInfo.Add(tempLang);
+            }
+
+            tempDilog.Navigation = InfoArray[i + 1, 3];
+            tempDilog.EndDialog = bool.Parse(InfoArray[i + 1, 4]);
+
+            dilogList.Add(tempDilog);
+        }
+    }
+
+
+    IEnumerator LoadSheet()
+    {
+        while(this.GetComponent<GoogleSheetManager>().data == null || this.GetComponent<GoogleSheetManager>().data == "")
+        {
+            yield return new WaitForSeconds(0.5f);
+            //print("Loading Dialog");
+        }
+
+        sheetData = this.GetComponent<GoogleSheetManager>().data;
+        //print(sheetData);
+        SetInfoList(sheetData);
+    }
+
+    int GetLanguageCount(string[,] dataArray, int ColumnSize)
+    {
+        int languageCount = 0;
+
+        for(int i=0; i< ColumnSize; i++)
+        {
+            if(dataArray[0,i].Contains("TEXT"))
+            {
+                //print(dataArray[0,i]);
+                int index = dataArray[0, i].IndexOf("_");
+
+                availableLanguages.Add(dataArray[0, i].Remove(0, index + 1));
+
+                languageCount++;
+            }
+        }
+
+        return languageCount;
+    }
+}
--- Assets/Scripts/EventSystem/GoogleSheetManager.cs
+++ Assets/Scripts/EventSystem/GoogleSheetManager.cs
@@ -2,24 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.IO;
 
 public class GoogleSheetManager : MonoBehaviour
 {
-    const string URL = "https://docs.google.com/spreadsheets/d/17l2xHpUDyDNRuh7rBE6A9BQmsleYnjUMd1S0OZfUM4g/export?format=tsv&range=A2:B";
+    public bool isUpdate;
 
+    public string URL;
 
+    public string folderName = "Dialog";
+    public string fileName = "dialog.tsv";
+    private string gameDataPath;
+
+    public string data;
+
+    public static GoogleSheetManager Singleton;
+    #region singleton
+    private void Awake()
+    {
+        if (null == Singleton)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this);
+        }
+        else Destroy(this);
+    }
+    #endregion
+
+    void Start()
+    {
+        data = null;
+
+        CreateGameInfoDir();
+        GetFileInfo();
+
+        /*
+        data = null;
+        if(data == null)
+        {
+            StartCoroutine(GetSheetInfo());
+        }
+        */
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
 
-    IEnumerator Start()
+    void CreateGameInfoDir()
+    {
+        gameDataPath = Application.persistentDataPath;
+        if (!Directory.Exists(gameDataPath + "/" + folderName + "/"))
+        {
+            print("folder doesn't exist, create folder");
+            Directory.CreateDirectory(Application.persistentDataPath + "/" + folderName + "/");
+        }
+    }
+
+    void GetFileInfo()
+    {
+        if(!File.Exists(gameDataPath + "/" + folderName + "/" + fileName) || isUpdate)
+        {
+            print("file doesn't exist, Import file or just Updating");
+
+            StartCoroutine(GetSheetInfo());
+        }
+        else
+        {
+            string path = gameDataPath + "/" + folderName + "/" + fileName;
+            //Read the text from directly from the test.txt file
+            StreamReader reader = new StreamReader(path);
+            data = reader.ReadToEnd();
+            //print("data reader: "+data);
+            reader.Close();
+        }
+    }
+
+    IEnumerator GetSheetInfo()
     {
         UnityWebRequest www = UnityWebRequest.Get(URL);
         yield return www.SendWebRequest();
 
-        string data = www.downloadHandler.text;
-        print(data);
+        data = www.downloadHandler.text;
+        //print(data);
+
+        //save data
+        string path = gameDataPath + "/" + folderName + "/" + fileName;
+
+        File.WriteAllText(path, data);
     }
 }
