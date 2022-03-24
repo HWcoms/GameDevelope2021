142926b3dda2e28f471dee80f83e72acc54f1038
HWcoms <dlgusdn383@gmail.com>
2022-03-24 20:38:56 +0900

    보스 씬 전환 (상호작용 키)

---
+++ Assets/Scripts/Mission/BossSceneEnterColider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossSceneEnterColider : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "BossDoorCol")
+        {
+            if(Input.GetButtonDown("Interact"))
+            {
+                SceneManager.LoadScene("BossScene");
+            }
+        }
+    }
+}
