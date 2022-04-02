9615190042367fba38224f36cc5ea9a596164b19
HWcoms <dlgusdn383@gmail.com>
2022-04-03 00:11:06 +0900

    AI navmesh 테스트 (창호)

---
+++ Assets/Navmesh Test (HW)/EnemyNavmeshTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavmeshTest : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+
+    public Transform dest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = dest.position;
+
+        if (Vector3.Distance(transform.position, dest.transform.position) > 5.0f)
+        {
+            navMeshAgent.isStopped = false;
+        }
+        else
+        {
+            navMeshAgent.isStopped = true;
+        }
+    }
+}
