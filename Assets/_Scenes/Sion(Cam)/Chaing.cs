e0f3919c34a1bee3040536b2769f3e6da445d57a
HWcoms <dlgusdn383@gmail.com>
2022-04-05 00:34:52 +0900

    카메라 전환(카메라 밝기 달라지는 버그) [시온]

---
+++ Assets/_Scenes/Sion(Cam)/Chaing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaing : MonoBehaviour
+{
+    float m_speed = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)==true)
+        {
+            gameObject.transform.Translate(new Vector3(0,0,0.01f));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, -0.01f));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            gameObject.transform.Translate(new Vector3(-0.01f, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            gameObject.transform.Translate(new Vector3(0.01f, 0, 0));
+        }
+    }
+}
---
+++ Assets/_Scenes/Sion(Cam)/FollowCamera.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public GameObject main_cam;
+    public GameObject sub_cam;
+    public GameObject subCameraPos;
+
+    public float lookAtSpeed = 2.0f;
+    public float moveSpeed = 1.5f;
+
+    [SerializeField] private bool isSubCam = false;
+    [SerializeField] private bool isButtonReady = true;
+
+    void Start()
+    {
+        isButtonReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("CameraChange") && isButtonReady)
+        {
+            StopAllCoroutines();
+            StartCoroutine(CameraChange());
+            isSubCam = !isSubCam;
+        }
+
+    }
+
+    IEnumerator CameraChange()
+    {
+        main_cam.SetActive(false);
+        sub_cam.SetActive(true);
+
+        isButtonReady = false;
+
+        if (isSubCam)
+        {
+            sub_cam.transform.position = subCameraPos.transform.position;
+
+            while ((Vector3.Distance(sub_cam.transform.position, main_cam.transform.position) > 0.1f) || Quaternion.Angle(sub_cam.transform.rotation, main_cam.transform.rotation) > 0.3f)
+            {
+                yield return null;
+                //print("changing position");
+
+                sub_cam.transform.position = Vector3.Lerp(sub_cam.transform.position, main_cam.transform.position, moveSpeed * Time.deltaTime);
+                sub_cam.transform.rotation = Quaternion.Slerp(sub_cam.transform.rotation, main_cam.transform.rotation, lookAtSpeed * Time.deltaTime);
+            }
+
+            sub_cam.transform.position = main_cam.transform.position;
+            sub_cam.transform.rotation = main_cam.transform.rotation;
+
+            main_cam.SetActive(true);
+            sub_cam.SetActive(false);
+        }
+        else if (!isSubCam)
+        {
+
+            sub_cam.transform.position = main_cam.transform.position;
+
+            while ((Vector3.Distance(sub_cam.transform.position, subCameraPos.transform.position) > 0.02f) || Quaternion.Angle(sub_cam.transform.rotation, subCameraPos.transform.rotation) > 0.1f)
+            {
+                yield return null;
+                //print("changing position");
+
+                sub_cam.transform.position = Vector3.Lerp(sub_cam.transform.position, subCameraPos.transform.position, moveSpeed * Time.deltaTime);
+                sub_cam.transform.rotation = Quaternion.Slerp(sub_cam.transform.rotation, subCameraPos.transform.rotation, lookAtSpeed * Time.deltaTime);
+            }
+
+            sub_cam.transform.position = subCameraPos.transform.position;
+            sub_cam.transform.rotation = subCameraPos.transform.rotation;
+        }
+
+        isButtonReady = true;
+    }
+}
